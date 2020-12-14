209551295040e3e1e6405c2b71a17dd835fd48eb
timot24 <timotiussimanjuntak24@gmail.com>
2020-12-14 20:17:44 +0700

    fixing error in aws

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -1,7 +1,7 @@
 const route = require('express').Router()
 const authMiddleware = require('../middleware/auth')
 const userController = require('../controllers/Users')
-const uploadAvatar = require('../controllers/uploadAvatar')
+const uploadAvatar = require('../controllers/UploadAvatar')
 
 route.get('/all', authMiddleware.authUser, userController.getAllUsers)
 route.get('/', authMiddleware.authUser, userController.getProfile)
