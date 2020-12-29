e570c61c2005b4f39de3a961d9826c453e748bfb
timot24 <timotiussimanjuntak24@gmail.com>
2020-12-29 16:45:41 +0700

    fix query

--- src/controllers/NewsArticle.js
+++ src/controllers/NewsArticle.js
@@ -81,7 +81,7 @@ module.exports = {
             ],
             [
               Sequelize.literal(`(
-                  IF(Likes.newsLiker = ${userId}, true, false)
+                IF(Likes.newsLiker = ${userId}, true, false) from Likes
                 )`),
               'isLiked'
             ],
